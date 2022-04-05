ed108cea7e8e95fa6e0558c5a8efd03a3cef6068
Neo <fullstackneo@gmail.com>
2022-04-05 00:11:16 -0600

    add get and delete route

--- server.js
+++ server.js
@@ -24,8 +24,6 @@ app.get('/', (req, res) => {
   res.json('Hello');
 });
 
-
-
 // Delete a candidate
 // db.query(`DELETE FROM candidates WHERE id = ?`, 5, (err, result) => {
 //   if (err) {
@@ -35,25 +33,79 @@ app.get('/', (req, res) => {
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [113, 'Ronald2', 'Firbank2', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [113, 'Ronald2', 'Firbank2', 1];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// db.query(`select * from candidates`, (err, rows) => {
+//   err && console.log(err.message);
+//   console.table(rows);
+// });
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows,
+    });
+  });
 });
 
-db.query(`select * from candidates`, (err, rows) => {
-  err && console.log(err.message);
-  console.table(rows);
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row,
+    });
+  });
 });
 
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found',
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
 
 app.use((req, res) => {
   res.status(404).end('213213');
+//   res.statusMessage(400);
   //   res.status(404);
   //   res.status(404).send('Not Found');
 });
