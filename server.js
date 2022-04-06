5cff5b9f3c667dd127902cc92d8bdd18dbd818d8
Neo <fullstackneo@gmail.com>
2022-04-06 14:22:06 -0600

    add voter routes

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
 const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json('Hello');
-});
-
 app.use('/api', apiRoutes);
-
-
-
-
 
 app.use((req, res) => {
   res.status(404).end('213213');
