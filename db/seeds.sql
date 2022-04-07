78f65a996f0bd60c21a83f2ff776101c72971d70
Neo <fullstackneo@gmail.com>
2022-04-06 23:09:42 -0600

    add votes route

--- db/seeds.sql
+++ db/seeds.sql
@@ -93,3 +93,17 @@ VALUES
   ),
   ('William', 'Carleton', 'wcarleton@literature.com'),
   ('Gerald', 'Griffin', 'ggriff@lit.net');
+
+INSERT INTO
+  votes (voter_id, candidate_id)
+VALUES
+  (3, 1),
+  (4, 2),
+  (5, 2),
+  (6, 2),
+  (7, 2),
+  (8, 3),
+  (9, 3),
+  (10,3),
+  (11,3),
+  (12,5);
